a4837fa65d9c129aa543d2d04471eb526100fd8a
tihanak <tihanakrivic94@gmail.com>
2023-01-18 11:28:41 +0100

    middleware example

---
+++ middleware-example/middleware-example/Program.cs
@@ -0,0 +1,13 @@
+using middleware_example.CustomMiddleware;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//Invoking custom middleware
+app.UseLoginMiddleware();
+
+app.Run(async context => {
+    await context.Response.WriteAsync("No response");
+});
+
+app.Run();
